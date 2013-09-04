e9d42ada05854b40253b9962e354322fbd69dfdd
Derek Knight (Mac) <Derek@GordonKnight.co.uk>
2013-09-05 11:30:07 +1200

    Greatly simplified Interface to Static module

--- lib/static.rb
+++ lib/static.rb
@@ -34,6 +34,30 @@ module Static
 		}
 	end
 
+	def Static.tableless_page(current_header, current_navbar, right_text, right_link, left_menu_items, error_message, yield_data)
+	server_root = ENV['SERVER_PORT'] == '80' ? ENV['SERVER_NAME'] : (ENV['SERVER_NAME'] + ':' + ENV['SERVER_PORT'])
+	url_root = 	'http://' + server_root + Rails::configuration.action_controller.relative_url_root
+	Static::header(right_text, url_root+right_link, current_header, current_navbar) +
+	'
+	  <div class="page-content">
+		<div class="left-menu">
+		  ' + Static::left_menu(url_root, left_menu_items) + '
+		</div>
+		<div class="right-content">
+		  <br/>
+		  <div class="error-content">
+			<p style="color: green">' + (error_message.nil? ? "" : error_message) + '</p>
+		  </div>
+		  <br/>
+		  <div class="page-data">
+			' + yield_data + '
+		  </div>
+		</div>
+	  </div>
+
+	  ' + Static::footer(url_root)
+	end
+
 	def Static.left_menu(url_root, left_menu_items)
 		rValue = '<ul>'
 		left_menu_items.each {|x| rValue += '<li><a href="' + url_root + (x[1]) + '">' + (x[0]) + '</a></li>' }
